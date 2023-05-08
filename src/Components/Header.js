845da32a943cb330c9bbeecb40d600545b08e6fb
Guilherme Gabriel Garcia <125367739+GuilhermeGGarcia12@users.noreply.github.com>
2023-05-08 02:07:19 -0300

    header feita

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,46 @@
 import Styles from "./Header.module.css";
+import Logo from "../assets/logoprincipal.png";
+import Olho from "../assets/olho.png";
+import Ganhos from "../assets/ganhos.png";
+import Perfil from "../assets/perfil.png";
+
 function Header(){
     return (
-        <div className={Styles.Header}>
-            
+    <div className={Styles.Header}>
+
+        <div className={Styles.Logocomnome}>
+            <img src={Logo} />
+            <a>Coinverse</a>
         </div>
+        
+        <div className={Styles.Operacoes}>
+            <nav>
+                <a>Aprenda</a>
+                <a>Invista</a>
+                <a>Opere na bolsa</a>
+                <a>Serviços</a>
+            </nav>
+        </div>
+
+
+        <div className={Styles.}>
+
+        <div className={Styles.Saldo}>
+            <img src={Olho} />
+            <p>Seu Saldo</p>
+        </div>
+
+        <div className={Styles.Mao}>
+            <img src={Ganhos} />
+        </div>
+
+        <div className={Styles.User}>
+            <img src={Perfil} />
+           <p>Entre</p> 
+        </div>
+        </div>
+
+    </div>
     )
 }
-export default Header 
+export default Header
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Header from './Components/Header';
 
-function App() {
+function Home() {
   return (
     <div className="App">
         <Header  />
@@ -9,4 +9,4 @@ function App() {
   );
 }
 
-export default App;
+export default Home;
